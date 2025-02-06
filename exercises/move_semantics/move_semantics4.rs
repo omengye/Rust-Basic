d099275df0bd7da71641505c1dd799d87daf125d
yemeng <yemeng@hatech.com.cn>
2025-02-06 09:39:22 +0800

    all exercises done

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
@@ -22,8 +20,8 @@ fn main() {
 }
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
-fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(v :Vec<i32>) -> Vec<i32> {
+    let mut vec = v;
 
     vec.push(22);
     vec.push(44);
